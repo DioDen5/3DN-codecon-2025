b16c0c730fe038db54665b69705ae05251c89c02
DenZastavnyi <denyszastavniy@gmail.com>
2025-10-18 18:25:35 +0300

    feat: add more teachers to demonstrate pagination

--- Backend/src/seed-teachers.js
+++ Backend/src/seed-teachers.js
@@ -35,6 +35,83 @@ const teachersData = [
         dislikes: 4,
         comments: 27,
         totalVotes: 48
+    },
+    {
+        name: 'Литвин Андрій Васильович',
+        university: 'ЛНУ імені Івана Франка',
+        subject: 'Інформатика',
+        image: '/assets/v7.jpg',
+        rating: 8.9,
+        likes: 28,
+        dislikes: 5,
+        comments: 19,
+        totalVotes: 33
+    },
+    {
+        name: 'Петренко Ольга Степанівна',
+        university: 'ЛНУ імені Івана Франка',
+        subject: 'Психологія',
+        image: '/assets/icon.jpg',
+        rating: 9.1,
+        likes: 39,
+        dislikes: 2,
+        comments: 25,
+        totalVotes: 41
+    },
+    {
+        name: 'Коваленко Максим Сергійович',
+        university: 'ЛНУ імені Івана Франка',
+        subject: 'Соціологія',
+        image: '/assets/icon.jpg',
+        rating: 8.4,
+        likes: 19,
+        dislikes: 6,
+        comments: 10,
+        totalVotes: 25
+    },
+    {
+        name: 'Ткачук Ірина Миколаївна',
+        university: 'ЛНУ імені Івана Франка',
+        subject: 'Англійська мова',
+        image: '/assets/icon.jpg',
+        rating: 9.0,
+        likes: 30,
+        dislikes: 3,
+        comments: 21,
+        totalVotes: 33
+    },
+    {
+        name: 'Гриценко Олег Іванович',
+        university: 'ЛНУ імені Івана Франка',
+        subject: 'Філософія',
+        image: '/assets/icon.jpg',
+        rating: 8.8,
+        likes: 25,
+        dislikes: 4,
+        comments: 15,
+        totalVotes: 29
+    },
+    {
+        name: 'Мельник Світлана Анатоліївна',
+        university: 'ЛНУ імені Івана Франка',
+        subject: 'Економіка',
+        image: '/assets/icon.jpg',
+        rating: 9.2,
+        likes: 41,
+        dislikes: 1,
+        comments: 30,
+        totalVotes: 42
+    },
+    {
+        name: 'Семенюк Володимир Степанович',
+        university: 'ЛНУ імені Івана Франка',
+        subject: 'Політологія',
+        image: '/assets/icon.jpg',
+        rating: 8.6,
+        likes: 20,
+        dislikes: 5,
+        comments: 12,
+        totalVotes: 25
     }
 ];
 
