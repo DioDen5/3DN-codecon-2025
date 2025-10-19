243a58ed883020d2f52b6f7a8c1e9e9c16554300
DenZastavnyi <denyszastavniy@gmail.com>
2025-10-19 19:42:06 +0300

    feat: add firstName and lastName to User model

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
     email:      { type: String, required: true, unique: true, index: true },
     passwordHash: { type: String, required: true },
     displayName:  { type: String, required: true },
+    firstName:  { type: String, required: true },
+    lastName:   { type: String, required: true },
     role:      { type: String, enum: ['student','admin'], default: 'student' },
     status:    { type: String, enum: ['pending','verified','rejected','suspended'], default: 'pending', index: true },
     isOwner:   { type: Boolean, default: false },
